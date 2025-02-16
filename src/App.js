d84a2d01fffc5f377cc10c717b6e07dd5e56a190
MoniOsorio <monica.osorioh@gmail.com>
2025-02-16 13:57:39 +0100

    installed and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>El clima</h1>
-      <footer>
-        This project was created by <a href="https://osorio.no/" target="_blank">Monica Osorio </a> {" "}and is {" "}
-        <a href="https://github.com/MoniOsorio/react25app" target="_blank">
-          open-sourced in Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>El clima</h1>
+        <footer>
+          This project was created by{" "}
+          <a href="https://osorio.no/" target="_blank">
+            Monica Osorio{" "}
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/MoniOsorio/react25app" target="_blank">
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
