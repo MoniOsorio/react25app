d04d286b4ee6d7138e57ce9cdec417f2e0d98cd0
MoniOsorio <monica.osorioh@gmail.com>
2025-02-16 13:32:37 +0100

    added references and links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      El clima
+      <h1>El clima</h1>
+      <footer>
+        This project was created by <a href="https://osorio.no/" target="_blank">Monica Osorio </a> {" "}and is {" "}
+        <a href="https://github.com/MoniOsorio/react25app" target="_blank">
+          open-sourced in Github
+        </a>
+      </footer>
     </div>
   );
 }
